67d0a1083508c1e28f49d97ebc6df5fbe2e7f294
wt_bruhhhh <59949135+ZH-XiJun@users.noreply.github.com>
2023-07-01 20:29:45 +0800

    add replace items

--- customize.sh
+++ customize.sh
@@ -1,2 +1,14 @@
 # 注意 这不是占位符！！这个代码的作用是将模块里的东西全部塞系统里，然后挂上默认权限
 SKIPUNZIP=0
+AUTOMOUNT=true
+REPLACE="
+/system/priv-app/Chat
+/system/priv-app/K8Contacts
+/system/priv-app/Makefriends
+/system/priv-app/VoiceAssistant
+/system/priv-app/K8Launcher
+/system/priv-app/Push
+/system/priv-app/SOS
+/system/priv-app/DataService
+/system/priv-app/AgoraVideo
+"
